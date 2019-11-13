4f569e2e97c2f0a0b974a8160f4ecb242f2e8b43
StasiyaB <baryash.anastasiya@gmail.com>
2019-11-13 13:24:12 +0100

    cycles

--- js tasks/js/recursion.js
+++ js tasks/js/recursion.js
@@ -2,8 +2,9 @@
 /* **************** Работа с рекурсией ************ */
 /*1. Дан массив с числами. Выведите последовательно его элементы используя рекурсию и не используя цикл.*/
 var arr = [1, 2, 3, 4, 5, 6];
-first(arr);
+//first(arr);
 //last(arr);
+func(arr);
 function first(arr) {
   document.write(arr.shift() + '<br>');
   if (arr.length != 0) {
@@ -16,7 +17,13 @@ function last(arr) {
     last(arr);
   }
 }
-
+function func(arr) {
+	alert(arr[0]);
+	arr.splice(0, 1);
+	if (arr.length > 0) {
+		func(arr);
+	}
+}
 /*2. Дано число. Сложите его цифры. Если сумма получилась более 9-ти, опять сложите его цифры. И так, пока сумма не станет однозначным числом (9 и менее).*/
 function reduceNum(num) {
   var sum = getDigitSum(num);
