30de67988319195378410191964bbd9137ce39a7
StasiyaB <baryash.anastasiya@gmail.com>
2019-12-25 00:00:21 +0100

    dom timer eventListener

--- js tasks/js/domTimer.js
+++ js tasks/js/domTimer.js
@@ -103,3 +103,103 @@ setInterval(function () {
 
 /* ********* Задачи для решения ********* */
 /*1.  Создайте отсчет от 0 до бесконечности:*/
+/*function start() {
+  window.setInterval(timer, 1000);
+}
+function timer() {
+  var elem = document.getElementById('count');
+  elem.innerHTML = parseInt(elem.innerHTML) + 1;
+}
+*/
+/*2.  Создайте отсчет с кнопкой остановки:*/
+/*function start() {
+  window.timerId = window.setInterval(timer, 1000);
+  var start = document.getElementById('start');
+  var stop = document.getElementById('stop');
+  start.disabled = true;
+  stop.disabled = false;
+}
+function timer() {
+  var elem = document.getElementById('count');
+  elem.innerHTML = parseInt(elem.innerHTML) + 1;
+}
+function stop() {
+  window.clearInterval(window.timerId);
+  var start = document.getElementById('start');
+  var stop = document.getElementById('stop');
+  start.disabled = false;
+  stop.disabled = true;
+}
+*/
+/*3. Создайте тикающие часики:*/
+/*
+<body onload="go()">
+	<div class="timer">
+		<p id="clock">
+			00:00:00
+		</p>
+	</div>
+</body>
+*/
+/*function go() {
+  window.timerId = window.setInterval(timer, 500);
+}
+
+function timer() {
+  var now = new Date();
+
+  var clock = document.getElementById('clock');
+  clock.innerHTML =  addZero(now.getHours()) +':'+ addZero(now.getMinutes()) +':'+ addZero(now.getSeconds());
+}
+
+function addZero(num) {
+  if (num <= 9) {
+    num = '0' + num;
+  }
+  return num;
+}
+*/
+/*4. Создайте таймер обратного отсчета:*/
+/*function start() {
+  window.timerId = window.setInterval(timer, 500);
+  var start      = document.getElementById('start');
+  start.disabled = true;
+}
+function stop() {
+  window.clearInterval(window.timerId);
+}
+function timer() {
+  var count     = document.getElementById('count');
+  var finished = document.getElementById('finished');
+
+  var number = parseInt(count.innerHTML) - 1;
+  count.innerHTML = number;
+
+    if (number == 0) {
+      stop();
+      finished.innerHTML = 'Обратный отсчет завершен.';
+    }
+
+}
+*/
+/*5. Создайте простой слайдер:*/
+var images = ['01.png', '02.png', '03.png'];
+var slider = document.querySelector('#slider');
+var img = slider.querySelector('img');
+var i = 0;
+
+function go() {
+  window.setInterval(function() {
+    img.src = 'img/' + images[i];
+    i ++;
+    if (i == images.length) {
+      i = 0;
+    }
+  }, 1000);
+}
+
+/*6. Создайте карусель:*/
+
+/*7. Создайте карусель:*/
+
+/*8.Создайте отсчет до полуночи:*/
